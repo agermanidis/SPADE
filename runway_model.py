c2c31ba49fd765789b734339046d3de6953c835b
Anastasis Germanidis <agermanidis@gmail.com>
2019-04-12 20:02:12 -0400

    add reference image as input

--- runway_model.py
+++ runway_model.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import torch
 import numpy as np
 import os
 import runway
@@ -14,11 +15,20 @@ class Options(BaseOptions):
         BaseOptions.initialize(self, parser)
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.add_argument('--how_many', type=int, default=float("inf"), help='how many test images to run')       
-        parser.set_defaults(preprocess_mode='scale_width_and_crop', crop_size=256, load_size=256, display_winsize=256)
         parser.set_defaults(serial_batches=True)
         parser.set_defaults(no_flip=True)
         parser.set_defaults(phase='test')
         parser.set_defaults(name='coco_pretrained')
+        parser.set_defaults(preprocess_mode='resize_and_crop')
+        parser.set_defaults(load_size=256)
+        parser.set_defaults(crop_size=256)
+        parser.set_defaults(display_winsize=256)
+        parser.set_defaults(label_nc=182)
+        parser.set_defaults(contain_dontcare_label=True)
+        parser.set_defaults(cache_filelist_read=True)
+        parser.set_defaults(cache_filelist_write=True)
+        if not torch.cuda.is_available():
+            parser.set_defaults(gpu_ids="-1")
         self.isTrain = False
         return parser
 
@@ -27,27 +37,24 @@ opt = Options().parse()
 @runway.setup(options={'checkpoints_root': runway.file()})
 def setup(opts):
     opt.checkpoints_dir = os.path.join(opts['checkpoints_root'], 'checkpoints')
-    if not torch.cuda.is_available():
-        opt.gpu_ids = -1
     model = Pix2PixModel(opt)
     model.eval()
     return model
 
 @runway.command('convert', inputs={'semantic_map': runway.image(channels=1), 'reference': runway.image}, outputs={'output': runway.image})
 def convert(model, inputs):
-    img = np.array(inputs['semantic_map'])
-    reference = inputs['reference']
-    h, w = img.shape[0:2]
-    img = Image.fromarray(img)
-    params = get_params(opt, (w, h))
+    img = inputs['semantic_map']
+    reference = inputs['reference'].convert('RGB')
+    params = get_params(opt, img.size)
     transform_label = get_transform(opt, params, method=Image.NEAREST, normalize=False)
-    label_tensor = transform_label(img).unsqueeze(0)
+    label_tensor = transform_label(img) * 255.0
+    label_tensor[label_tensor == 255.0] = opt.label_nc
     transform_image = get_transform(opt, params)
     image_tensor = transform_image(reference)
     data = {
-        'label': label_tensor,
-        'instance': label_tensor,
-        'image': image_tensor
+        'label': label_tensor.unsqueeze(0),
+        'instance': label_tensor.unsqueeze(0),
+        'image': image_tensor.unsqueeze(0)
     }
     generated = model(data, mode='inference')
     output = util.tensor2im(generated[0])
